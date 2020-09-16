32df5b66b91762feda16b85b26d5ecebea11d61d
qpalzm88 <qpalzm@list.ru>
2020-09-16 21:07:43 +0300

    добавил оценки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 
-export class Lesson{
+export class Lesson {
   constructor(
- public number: number, 
- public date: Date, 
- public theme:string,
- public homework:string,
- public note: string)
-  {}
+    public number: number,
+    public date: Date,
+    public theme: string,
+    public homework: string,
+    public note: string) { }
+}
+
+export class Student {
+  public fio: string;
+  public rait: Array<number> = [];
+  public raiting: number;
+  public avrg: number;
+
+  constructor(value: string) {
+    this.fio = value
+  }
+
+  setFio(value: string) {
+    this.fio = value;
+  }
+
+  addRait(r: number) {
+    this.rait.push(r);
+  }
+
+
 }
 
 @Component({
@@ -16,11 +36,25 @@ export class Lesson{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lesson: Lesson = new Lesson(1, new Date(), "Тема урока",  "Домашнее задание",  "Примечание");
-  lessons:Lesson[]=[];
+  lesson: Lesson = new Lesson(1, new Date(), "Тема урока", "Домашнее задание", "Примечание");
+  lessons: Lesson[] = [];
+  students: Student[] = [new Student("Иванов"), new Student("Петров"), new Student("Сидоров")];
+  raitinggs: number[] = [1, 2, 3, 4, 5];
 
-  addLesson(){
-    this.lessons.push(new Lesson(this.lesson.number, this.lesson.date, this.lesson.theme, this.lesson.homework, this.lesson.note));
+  addLesson() {
+    var lesson = new Lesson(this.lesson.number, this.lesson.date, this.lesson.theme, this.lesson.homework, this.lesson.note);
+    this.lessons.push(lesson);
     this.lessons = JSON.parse(localStorage.getItem('lesson')).map((this.lesson));
+  }
+
+  addRait(ra: number, student: Student) {
+    student.addRait(ra);
+    let i: number = 0;
+    for (let index = 0; index < student.rait.length; index++) {
+      let r: number = student.rait[index];
+      i = i*1 + r*1;
+    }
+    student.raiting =Math.floor(i / student.rait.length * 100) / 100;
+    student.avrg = Math.round(i / student.rait.length);
   }
 }

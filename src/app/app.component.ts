6af6a51820e8f49d3761ef51d40b5bfee1569969
qpalzm88 <qpalzm@list.ru>
2020-09-16 17:01:11 +0300

    table lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Lesson{
+  constructor(
+ public number: number, 
+ public date: Date, 
+ public theme:string,
+ public homework:string,
+ public note: string)
+  {}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'home4th1st';
+  lesson: Lesson = new Lesson(1, new Date(), "Тема урока",  "Домашнее задание",  "Примечание");
+  lessons:Lesson[]=[];
+
+  addLesson(){
+    this.lessons.push(new Lesson(this.lesson.number, this.lesson.date, this.lesson.theme, this.lesson.homework, this.lesson.note));
+    this.lessons = JSON.parse(localStorage.getItem('lesson')).map((this.lesson));
+  }
 }
